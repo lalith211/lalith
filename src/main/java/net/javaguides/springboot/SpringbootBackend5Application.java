66357c3b541bc7cc74d2799f13fd52d7292fe271
lalith211 <lalithkumar.surada@imaginnovate.com>
2022-06-30 20:48:30 +0530

    end points have been added for posts table

--- src/main/java/net/javaguides/springboot/SpringbootBackend5Application.java
+++ src/main/java/net/javaguides/springboot/SpringbootBackend5Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringbootBackend3Application {
+public class SpringbootBackend5Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringbootBackend3Application.class, args);
+		SpringApplication.run(SpringbootBackend5Application.class, args);
 	}
 
 }
